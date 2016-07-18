6d7c918e8258b1d28b0b09a9d970f47263f5e9ba
hobbistas <dominakos@gmail.com>
2016-07-18 23:28:02 +0200

    Add files via upload

---
+++ MobilePhone.java
@@ -0,0 +1,19 @@
+
+
+//import static homework.test.*;
+
+public class MobilePhone {
+
+	public static void main (String[] argv){
+		
+		Accessor accessNow = new Accessor();
+		accessNow.getIt();
+		System.exit(0);
+		
+	}
+}
+
+
+	
+
+
